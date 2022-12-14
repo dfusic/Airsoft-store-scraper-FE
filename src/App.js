c2880041c1ca664da0e8a4d90659109e5c922c49
Duro Fusic <d.fusic@gmail.com>
2022-12-14 15:48:02 +0100

    initial fe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import axios from "axios";
+import "./App.css"
+const App = () => {
 
-function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
+
+  const handleInputChange = (e) => setInputValue(e.target.value)
+  const handleSearch = () => {
+    axios.get(`http://localhost:3000/?search=${inputValue}`).then(response => {
+      setItems(response.data);
+    })
+  }
+  const handleSort = (e) => {
+    axios.get(`http://localhost:3000/?sort=${e.target.value}`).then(response => {
+      setItems(response.data);
+    })
+  }
+  const handleReset = () => {
+    axios.get('http://localhost:3000/').then(response => {
+      setItems(response.data);
+    })
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/').then(response => {
+      setItems(response.data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hristo scraper</h1>
+      <input type="text" onChange={handleInputChange} value={inputValue} />
+      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleSort} value='asc'>Ascending</button>
+      <button onClick={handleSort} value='desc'>Descending</button>
+      <button onClick={handleReset}>Reset filters</button>
+      <div id="hristo">
+      {items.map(item => (
+            <ul className='item'>
+              <li className='item-title' style={{color: item.available ? 'green' : 'red'}}>{item.title}</li>
+              <li>{item.priceEuro}</li>
+              <li>discounted: {String(item.isDiscounted)}</li>
+              <li>available: {String(item.available)}</li>
+              <li>
+                <a href={item.url} target='_blank'>{item.url}</a>
+              </li>
+            </ul>
+        ))}
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
